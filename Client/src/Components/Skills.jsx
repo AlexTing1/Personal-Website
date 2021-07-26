51bba5abb2d625c9314469662447400af82e36d4
Alex Ting <alexting6366@gmail.com>
2021-07-25 23:37:55 -0700

    Added Backend list to tech list

--- Client/src/Components/Skills.jsx
+++ Client/src/Components/Skills.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SkillsList from './SkillsList';
+import css from './css/skills.css';
 
 function Skills() {
   const frontEnd = [
@@ -30,13 +31,51 @@ function Skills() {
   ];
 
   const backEnd = [
-
+    {
+      name: 'Node.js',
+      image: 'https://e7.pngegg.com/pngimages/130/641/png-clipart-javascript-node-js-computer-icons-logo-application-software-javascript-icon-text-logo.png',
+    },
+    {
+      name: 'Express',
+      image: 'https://e7.pngegg.com/pngimages/130/641/png-clipart-javascript-node-js-computer-icons-logo-application-software-javascript-icon-text-logo.png',
+    },
+    {
+      name: 'MySQL',
+      image: 'https://e7.pngegg.com/pngimages/252/959/png-clipart-mysql-database-server-microsoft-sql-server-others-text-logo.png',
+    },
+    {
+      name: 'PostgreSQL',
+      image: 'https://w7.pngwing.com/pngs/89/118/png-transparent-computer-icons-postgresql-database-others-logo-head-silhouette-thumbnail.png',
+    },
+    {
+      name: 'MongoDB',
+      image: 'https://w7.pngwing.com/pngs/292/793/png-transparent-mongodb-logo-node-js-computer-icons-fontshop-international-angle-logo-black.png',
+    },
+    {
+      name: 'Mongoose',
+      image: 'https://w7.pngwing.com/pngs/292/793/png-transparent-mongodb-logo-node-js-computer-icons-fontshop-international-angle-logo-black.png',
+    },
+    {
+      name: 'Firebase',
+      image: 'https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/firebase-512.png',
+    },
+    {
+      name: 'REST APIs',
+      image: 'https://e7.pngegg.com/pngimages/592/502/png-clipart-logo-application-programming-interface-computer-programming-application-software-api-text-photography-thumbnail.png',
+    },
   ];
 
   return (
     <div>
       <h1>Tech Skills</h1>
-      <SkillsList list={frontEnd} />
+      <div className={css.container}>
+        <div className={css.front}>
+          <SkillsList list={frontEnd} name="Frontend" />
+        </div>
+        <div className={css.back}>
+          <SkillsList list={backEnd} name="Backend" />
+        </div>
+      </div>
     </div>
   );
 }
--- Client/src/Components/SkillsList.jsx
+++ Client/src/Components/SkillsList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import css from './css/skillsList.css';
 
-function SkillsList({ list }) {
+function SkillsList({ list, name }) {
   return (
     <div>
+      <h1>{name}</h1>
       {list.map((data) => (
         <div className={css.block}>
           <img src={data.image} alt="img" className={css.image} />
