6d87f2a678f5078b47830d237be55f4e1a97fb91
Alex Ting <alexting6366@gmail.com>
2021-08-06 23:40:46 -0700

    Testing Modal

--- Client/src/Components/ProjectTile.jsx
+++ Client/src/Components/ProjectTile.jsx
@@ -48,8 +48,33 @@ function ProjectTile({ tile }) {
         isOpen={isModalOpen}
         onRequestClose={closeModal}
       >
-        <span>TESTING MODAL</span>
-        <button type="button" onClick={closeModal}>Close Modal</button>
+        <div id="tile image" className={css.imgContainer}>
+          <img src={tile.imgDisplay} alt="default display" className={css.img} />
+        </div>
+        <div id="text block" className={css.textBlock}>
+          <div className={css.textComponent}>
+            <h2>{tile.title}</h2>
+          </div>
+          <div className={css.textComponent}>
+            <p>{tile.body}</p>
+          </div>
+          <div className={css.textComponent}>
+            <TechList list={tile.tech} />
+          </div>
+          <div>
+            <a href={tile.github} className={css.link}>
+              <button type="button" className={css.myButton}>
+                <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="github" className={css.iconLinks} />
+                <span className={css.text}>
+                  {tile.title}
+                  {' '}
+                  Github
+                </span>
+              </button>
+            </a>
+            <button type="button" onClick={openModal}>Open Modal</button>
+          </div>
+        </div>
       </Modal>
     </div>
   );
