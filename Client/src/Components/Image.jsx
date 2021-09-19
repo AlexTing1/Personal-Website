2ff14fa1f1f06adedf7a3855e3c73fea0497b203
Alex Ting <alexting6366@gmail.com>
2021-09-19 01:07:45 -0700

    fixed title text of website

--- Client/src/Components/Image.jsx
+++ Client/src/Components/Image.jsx
@@ -5,7 +5,11 @@ function Image() {
   return (
     <div id="image" className={css.container}>
       <img className={css.image} src="https://www.tahiti.com/images1/thumbs/BOBILM-Beach-1200x720.jpg" alt="borabora" />
-      <h2 className={css.imageText}>Alex Ting</h2>
+      <div className={css.imageTextContainer}>
+        <h1 className={css.imageTextName}>Alexander Ting</h1>
+        <h2 className={css.imageTextTitle}>Full-stack Software Engineer</h2>
+      </div>
+
     </div>
   );
 }
